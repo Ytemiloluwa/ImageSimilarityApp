b11cf09c981a946bec7b7e68791d2e598e301fe8
Temiloluwa <91268094+Ytemiloluwa@users.noreply.github.com>
2022-08-26 14:29:20 +0100

    initial commits

--- ImageSimilarityApp/ButtonLabel.swift
+++ ImageSimilarityApp/ButtonLabel.swift
@@ -4,5 +4,35 @@
 //
 //  Created by Temiloluwa on 26/08/2022.
 //
-
 import Foundation
+import SwiftUI
+import UIKit
+
+
+struct ButtonLabel: View {
+    
+    private let text: String
+    private let background: Color
+    
+    var body: some View {
+        
+        HStack {
+            
+            Spacer()
+            
+            Text(text).font(.title).bold().foregroundColor(.white)
+                
+            
+            Spacer()
+            
+            
+        }.padding().background(background).cornerRadius(10)
+        .frame(width: 200, height: 100)
+    }
+    init(text: String, background: Color) {
+        
+        self.text = text
+        self.background = background
+    }
+    
+}
