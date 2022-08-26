04e7ba28e469dba00db0cde0ebf8517f4e999b80
Temiloluwa <91268094+Ytemiloluwa@users.noreply.github.com>
2022-08-26 13:32:25 +0100

    Initial Commit

---
+++ ImageSimilarityApp/ImageSimilarityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSimilarityAppApp.swift
+//  ImageSimilarityApp
+//
+//  Created by Temiloluwa on 26/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ImageSimilarityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
