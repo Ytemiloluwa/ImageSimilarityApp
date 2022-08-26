69f54ad8085ddb42d6ce1ea0de8695891ab98352
Temiloluwa <91268094+Ytemiloluwa@users.noreply.github.com>
2022-08-26 14:35:36 +0100

    initial commit

--- ImageSimilarityApp/ImagePickerView.swift
+++ ImageSimilarityApp/ImagePickerView.swift
@@ -10,10 +10,92 @@ import SwiftUI
 import UIKit
 
 
-struct ImagePickerView {
+struct ImagePickerView: View {
     
     private let completion: (UIImage?) -> ()
     private var camera: Bool
     
+    var body: some View {
+        
+       ImagePickerControllerWrapper(camera: camera, completion: completion)
+    }
     
+    init(camera: Bool = false, completion: @escaping (UIImage?) -> ()) {
+        
+        self.camera = camera
+        self.completion = completion
+    }
+}
+
+
+struct ImagePickerControllerWrapper: UIViewControllerRepresentable {
+    
+    typealias UIViewControllerType = UIImagePickerController
+    
+    private(set) var selectedImage: UIImage?
+    private(set) var cameraSource: Bool
+    
+    private let completion: (UIImage?) -> ()
+    
+    init(camera: Bool, completion: @escaping (UIImage?) -> ()) {
+        
+        self.cameraSource = camera
+        self.completion = completion
+        
+    }
+    
+    func makeCoordinator() -> ImagePickerControllerWrapper.Coordinator {
+        
+        let coordinator = Coordinator(self)
+        coordinator.completion = self.completion
+        
+        return coordinator
+    }
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        
+        let imagepickerController = UIImagePickerController()
+        imagepickerController.delegate = context.coordinator
+        imagepickerController.sourceType = cameraSource ? .camera : .photoLibrary
+        
+        return imagepickerController
+        
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        
+        var parent: ImagePickerControllerWrapper
+        var completion : ((UIImage?) -> ())?
+        
+        init(_ imagePickerControllerWrapper: ImagePickerControllerWrapper) {
+            
+            self.parent = imagePickerControllerWrapper
+        }
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        
+        print("Image Picker Complete")
+        
+        let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        
+        picker.dismiss(animated: true)
+        
+        completion(selectedImage)
+        
+        
+    }
+    func imageControllerDidCancel(_ picker: UIImagePickerController) {
+        
+        print("Imagen Picker Cancel")
+        picker.dismiss(animated: true)
+        completion(nil)
+        
+        
+    }
 }
